a485a42ff217831c01376f7e2ca2f46f89197c1d
apoorv-chandra <apoorvchandra@gmail.com>
2019-06-19 17:25:00 +0530

    Login component and Angular material library added.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,16 @@ import {NgModule} from '@angular/core';
 import {Routes, RouterModule} from '@angular/router';
 import {FrontPageComponent} from "./BaseComponents/FrontPage/front-page.component";
 import {ContactUsComponent} from "./BaseComponents/FrontPage/contact-us.component";
+import {LoginComponent} from "./BaseComponents/login/login.component";
 
 const routes: Routes = [{
-  path: '', component: FrontPageComponent, children: [{path: 'ContactUs', component: ContactUsComponent}]
+  path: '', component: FrontPageComponent,
+  children: [
+    {path: 'ContactUs',
+      component: ContactUsComponent},
+    {path: 'Login',
+    component: LoginComponent}
+      ]
 }];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { FrontPageComponent } from './BaseComponents/FrontPage/front-page.component';
 import { ContactUsComponent } from './BaseComponents/FrontPage/contact-us.component';
 import { AboutUsComponent } from './BaseComponents/FrontPage/about-us.component';
+import { LoginComponent } from './BaseComponents/login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     FrontPageComponent,
     ContactUsComponent,
-    AboutUsComponent
+    AboutUsComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
