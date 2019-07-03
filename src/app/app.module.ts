85d3c322a728ed204bac017345c8a3ea1e759197
sid12261996 <sidharthrkc@gmail.com>
2019-07-03 12:20:24 +0530

    Merge branch 'master' of C:\Users\Sidharth\Desktop\SidProjects\Nora Tech\Nora Tech site\NoraTech with conflicts.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,15 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { FrontPageComponent } from './BaseComponents/FrontPage/front-page.component';
-import { ContactUsComponent } from './BaseComponents/FrontPage/contact-us.component';
-import { AboutUsComponent } from './BaseComponents/FrontPage/about-us.component';
-import { LoginComponent } from './BaseComponents/login/login.component';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {FrontPageComponent} from './BaseComponents/FrontPage/front-page.component';
+import {ContactUsComponent} from './BaseComponents/FrontPage/contact-us.component';
+import {AboutUsComponent} from './BaseComponents/FrontPage/about-us.component';
+import {LoginComponent} from './login/login.component';
+import {HttpClient, HttpClientModule} from '@angular/common/http';
+import {ReactiveFormsModule} from '@angular/forms';
+import {MatButtonModule, MatCardModule, MatFormFieldModule, MatInputModule, MatSelectModule} from '@angular/material';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -18,9 +21,25 @@ import { LoginComponent } from './BaseComponents/login/login.component';
   ],
   imports: [
     BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+    ReactiveFormsModule,
+    ReactiveFormsModule,
+    ReactiveFormsModule,
+    ReactiveFormsModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatCardModule,
+    MatInputModule,
+    MatButtonModule,
     AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
