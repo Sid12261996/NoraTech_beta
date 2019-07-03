3f6f42b4b079e4a0dd2f0f383348add835c16a85
sid12261996 <sidharthrkc@gmail.com>
2019-07-03 13:07:04 +0530

    Routes added and official info@noratechnologies.com set to recipient

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ var express  = require('express'),
     cors = require('cors'),
     bodyParser= require('body-parser'),
     path = require('path'),
-    nodemailer = require('nodemailer')
+    nodemailer = require('nodemailer'),
+    mailer = require('./routes/mailer')
 ;
 app.use(cors());
 
@@ -15,33 +16,7 @@ const testAccount={
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-
-async function sendMail(subject,body, res){
-    let info = await transporter.sendMail({
-        from: 'noratechsolutionspvtltd@gmail.com', // sender address
-        to: "sidharthrkc@gmail.com", // list of receivers
-        subject: subject, // Subject line
-        text: body, // plain text body
-        // html: "<b>Hello world?</b>" // html body
-    });
-    if(info.accepted!==null&&info.accepted!==[]){
-        res.status(200).json({message:'Successfully sent the mail!!'})
-    }
-
-}
-const transporter = nodemailer.createTransport({
-    auth: {
-        user: testAccount.user, // generated ethereal user
-        pass: testAccount.pass // generated ethereal password
-    },
-
-    service: 'gmail'
-});
-
-app.post('/mail', async (req, res) => {
-    await sendMail(req.body.subject, req.body.body,res);
-
-})
+app.use('/mail', mailer);
 
 
 app.get('/*',(req,res)=>{
---
+++ routes/mailer.js
@@ -0,0 +1,37 @@
+var Router = require('express').Router(),
+    nodemailer = require('nodemailer');
+
+
+const testAccount={
+    user:'noratechsolutionspvtltd@gmail.com',
+    pass:'noraAsdf@123'
+}
+
+async function sendMail(subject,body, res){
+    let info = await transporter.sendMail({
+        from: 'noratechsolutionspvtltd@gmail.com', // sender address
+        to: "sidharthrkc@gmail.com;info@noratechnologies.com", // list of receivers
+        subject: subject, // Subject line
+        text: body, // plain text body
+
+    });
+    if(info.accepted!==null&&info.accepted!==[]){
+        res.status(200).json({message:'Successfully sent the mail!!'})
+    }
+
+}
+const transporter = nodemailer.createTransport({
+    auth: {
+        user: testAccount.user, // generated ethereal user
+        pass: testAccount.pass // generated ethereal password
+    },
+
+    service: 'gmail'
+});
+
+Router.post('',async (req, res) => {
+    await sendMail(req.body.subject, req.body.body,res);
+
+})
+
+module.exports= Router;
